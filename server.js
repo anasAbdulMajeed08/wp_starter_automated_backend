d9a875b3fb00e9d14d541d8a9a79b9592ce4924c
Anas <anasabdulmajeed08@gmail.com>
2021-07-12 22:43:42 +0300

    changed the port

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ app.get("/", (req, res) => {
 });
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
-const port = 8000;
+const port = "https://wp-starter-automated-backend.vercel.app/";
 app.listen(port, () => console.log(`server is running in port ${port}`));
